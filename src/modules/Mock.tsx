3533849e7cca74cb55e13dd49fa54adc988f8ae4
khristina <hristinakirillova2559@gmail.com>
2024-02-13 19:41:47 +0300

    g

--- src/modules/Mock.tsx
+++ src/modules/Mock.tsx
@@ -16,13 +16,13 @@ export const mockModelings: Modeling[] = [
         modelingId : 0,
         name : 'Аналитическое моделирование очереди в узле сети',
         description: 'Рассчет времени ожидания в узле сети',
-        image: '',
+        image: 'https://khristina455.github.io/NetTestReact/blob/gh-pages/default.webp',
         price: 50000,
     }, {
         modelingId: 1,
         name: 'Аналитическое моедлирование прохождения сообщения в сети',
         description: "Рассчет среденего времени прохождения сообщения в сети",
-        image: '',
+        image: 'https://khristina455.github.io/NetTestReact/blob/gh-pages/default.webp',
         price: 90000,
     }
 ]
