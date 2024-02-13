36987a309e790cba6762ed21fb27da3c599b41d1
khristina <hristinakirillova2559@gmail.com>
2024-02-13 19:20:45 +0300

    fix image

--- src/modules/Mock.tsx
+++ src/modules/Mock.tsx
@@ -16,13 +16,13 @@ export const mockModelings: Modeling[] = [
         modelingId : 0,
         name : 'Аналитическое моделирование очереди в узле сети',
         description: 'Рассчет времени ожидания в узле сети',
-        image: '/default.webp',
+        image: '',
         price: 50000,
     }, {
         modelingId: 1,
         name: 'Аналитическое моедлирование прохождения сообщения в сети',
         description: "Рассчет среденего времени прохождения сообщения в сети",
-        image: '/default.webp',
+        image: '',
         price: 90000,
     }
 ]
